87f0e96962b8bcfdb24fc241b2cd3a5f9276e908
metrosmash <ajiboyetolu1@gmail.com>
2026-08-22 14:13:41 +0100

    feat(main): added the main checker and tested it

--- workbook_one/main.py
+++ workbook_one/main.py
@@ -7,15 +7,21 @@ from teammate_two.subtract import subtraction
 
 MIN = 5
 
+# need to find a better function test to check if both users have tried git on thier end
+def test_collaboration(a, b):
+
+    teamate_one_add = addition(a, b)
+    
+    teamate_two_subtract = subtraction(a, b)
+    
+    
+    if teamate_one_add > teamate_two_subtract:
+        print("Passed: Addition is greater than Subtraction")
+    else:
+        print("Failed: Addition is not greater than Subtraction")
 
 
 if __name__ == "__main__":
-    total = addition(2, 2) + subtraction(2,4)
-    
-    check = max (MIN,total)
-
-    if check == 5:
-        print("Failed")
-    else:
-        print("passed")
-
+    a = 10
+    b = 5 
+    test_collaboration(a, b)
