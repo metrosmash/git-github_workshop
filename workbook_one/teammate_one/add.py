c3c8cd79593f9297bfcfc5dff0e4a1d201041da4
metrosmash <ajiboyetolu1@gmail.com>
2026-08-22 13:42:05 +0100

    feat(answer): added the addition and subtraction files for testing

---
+++ workbook_one/teammate_one/add.py
@@ -0,0 +1,4 @@
+# addition function to add two numbers together 
+
+def addition(a, b):
+    return a + b
---
+++ workbook_one/teammate_two/subtract.py
@@ -0,0 +1,5 @@
+# Creating the subtraction function 
+
+def subtraction(a, b):
+    return a - b
+
