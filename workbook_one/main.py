8a50d5c522b4e0fe97d4b9a77c47f92c1187e638
metrosmash <ajiboyetolu1@gmail.com>
2026-08-22 15:33:18 +0100

    fix: deleted some depreciated lines

--- workbook_one/main.py
+++ workbook_one/main.py
@@ -5,17 +5,30 @@ NOTE: do not modify this code do your tasks on your folders
 from teammate_one.add import addition
 from teammate_two.subtract import subtraction
 
-MIN = 5
+"""
+NOTE: do not modify this code do your tasks on your folders 
+"""
 
+from teammate_one.add import addition
+from teammate_two.subtract import subtraction
+
+
+# explanation on the testing and the criteron here 
+def test_collaboration(a, b):
+
+    teamate_one_add = addition(a, b)
+    
+    teamate_two_subtract = subtraction(a, b)
+    
+    
+    if teamate_one_add > teamate_two_subtract:
+        print("Passed: Addition is greater than Subtraction")
+    else:
+        print("Failed: Addition is not greater than Subtraction")
 
 
 if __name__ == "__main__":
-    total = addition(2, 2) + subtraction(2,4)
-    
-    check = max (MIN,total)
-
-    if check == 5:
-        print("Failed")
-    else:
-        print("passed")
+    a = 10
+    b = 5 
+    test_collaboration(a, b)
 
